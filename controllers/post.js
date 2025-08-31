0a6fda5ad2a1007bd751d0a5f3407b602283e3ec
Mohammad Anas <anas092121@gmail.com>
2025-08-31 13:42:31 +0530

    Error handling added in post controller

--- controllers/post.js
+++ controllers/post.js
@@ -1,114 +1,112 @@
 import { Post } from "../models/post.js";
-import { User } from "../models/user.js";
+import ErrorHandler from "../middleWares/errorHandler.js";
 
-//
-// create new post
-export const createPost = async (req, res) => {
-  const { title, caption, image, tags } = req.body;
+// Create new post
+export const createPost = async (req, res, next) => {
+  try {
+    const { title, caption, image, tags } = req.body;
 
-  const post = await Post.create({
-    user: req.user._id,
-    title,
-    caption,
-    image,
-    tags,
-  });
+    if (!title || !caption)
+      return next(new ErrorHandler("Title and caption are required", 400));
 
-  res.status(201).json({
-    success: true,
-    message: "Post created successfully",
-    post,
-  });
+    const post = await Post.create({
+      user: req.user._id,
+      title,
+      caption,
+      image,
+      tags,
+    });
+
+    res.status(201).json({
+      success: true,
+      message: "Post created successfully",
+      post,
+    });
+  } catch (err) {
+    next(err); // All unexpected errors handled by global middleware
+  }
 };
 
-//
-// get all posts of a user
-export const getAllPosts = async (req, res) => {
-  const posts = await Post.find()
-    .populate({ path: "user", select: "name" })
-    .sort({ createdAt: -1 });
+// Get all posts
+export const getAllPosts = async (req, res, next) => {
+  try {
+    const posts = await Post.find()
+      .populate({ path: "user", select: "name" })
+      .sort({ createdAt: -1 });
 
-  res.json({
-    success: true,
-    posts,
-  });
+    res.status(200).json({
+      success: true,
+      posts,
+    });
+  } catch (err) {
+    next(err);
+  }
 };
 
-//
-// get only one post with id
-export const getPostById = async (req, res) => {
-  const { id } = req.params;
-
-  const post = await Post.findById(id).populate({
-    path: "user",
-    select: "name",
-  });
-
-  if (!post) {
-    return res.status(404).json({
-      success: false,
-      message: "Post Not Found",
+// Get a single post by ID
+export const getPostById = async (req, res, next) => {
+  try {
+    const post = await Post.findById(req.params.id).populate({
+      path: "user",
+      select: "name",
     });
-  }
 
-  res.status(200).json({
-    success: true,
-    post,
-  });
+    if (!post) return next(new ErrorHandler("Post not found", 404));
+
+    res.status(200).json({
+      success: true,
+      post,
+    });
+  } catch (err) {
+    next(err);
+  }
 };
 
-export const updatePost = async (req, res) => {
-  const { id } = req.params;
-  const { title, caption, image, tags } = req.body;
-  const post = await Post.findById(id);
-  if (!post) {
-    return res.status(404).json({
-      success: false,
-      message: "Post not found",
-    });
-  }
-  // checking ownership of post
-  if (post.user.toString() !== req.user._id.toString()) {
-    return res.status(401).json({
-      success: false,
-      message: "Unauthorised",
-    });
-  }
+// Update a post
+export const updatePost = async (req, res, next) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) return next(new ErrorHandler("Post not found", 404));
 
-  post.title = title || post.title;
-  post.caption = caption || post.caption;
-  post.image = image || post.image;
-  post.tags = tags || post.tags;
-  await post.save();
+    // Check ownership
+    if (post.user.toString() !== req.user._id.toString())
+      return next(new ErrorHandler("Unauthorized", 403));
 
-  res.status(200).json({
-    success: true,
-    message: "Post updated successfully",
-    post,
-  });
+    const { title, caption, image, tags } = req.body;
+    post.title = title || post.title;
+    post.caption = caption || post.caption;
+    post.image = image || post.image;
+    post.tags = tags || post.tags;
+
+    await post.save();
+
+    res.status(200).json({
+      success: true,
+      message: "Post updated successfully",
+      post,
+    });
+  } catch (err) {
+    next(err);
+  }
 };
 
-export const deletePost = async (req, res) => {
-  const { id } = req.params;
-  const post = await Post.findById(id);
-  if (!post) {
-    return res.status(404).json({
-      success: false,
-      message: "Post not found",
-    });
-  }
+// Delete a post
+export const deletePost = async (req, res, next) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) return next(new ErrorHandler("Post not found", 404));
 
-  // checking the ownership of post
-  if (post.user.toString() !== req.user._id.toString()) {
-    return res.status(403).json({
-      success: false,
-      message: "Not authorized to delete this post",
-    });
-  }
+    // Check ownership
+    if (post.user.toString() !== req.user._id.toString())
+      return next(new ErrorHandler("Not authorized to delete this post", 403));
 
-  await post.deleteOne();
-  res.status(200).json({
-    success: true,
-    message: "Post Deleted Successfully",
-  });
+    await post.deleteOne();
+
+    res.status(200).json({
+      success: true,
+      message: "Post deleted successfully",
+    });
+  } catch (err) {
+    next(err);
+  }
 };
