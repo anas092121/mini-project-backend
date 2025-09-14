55c21be8737b708955ad4bb142d2fbae06cfda11
Mohammad Anas <anas092121@gmail.com>
2025-09-15 00:26:51 +0530

    two config files added -> cloudinary.js and multer.js <- for image uploads

---
+++ config/cloudinary.js
@@ -0,0 +1,9 @@
+import { v2 as cloudinary } from "cloudinary";
+
+cloudinary.config({
+  cloud_name: "devanascloud",
+  api_key: "463728187263194",
+  api_secret: "FyT0zEUSWuKvwMPb_9dz6lLHKII",
+});
+
+export default cloudinary;
